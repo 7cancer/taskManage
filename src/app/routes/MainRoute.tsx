4e724ae9b5e663570737466a61389bdb7c048378
Takahiro Arai <33454308+7cancer@users.noreply.github.com>
2026-03-08 20:53:18 +0900

    Update MainRoute.tsx

--- src/app/routes/MainRoute.tsx
+++ src/app/routes/MainRoute.tsx
@@ -1,4 +1,5 @@
 import { CsvImportDialog } from '../../features/csv-import/components/CsvImportDialog';
+import { MainLayout } from '../layout/MainLayout';
 
 export function MainRoute() {
   // TODO: 画面タブ切り替え（カンバン/ガント）を実装する。
@@ -6,5 +7,6 @@ export function MainRoute() {
     <MainLayout>
       <p>TODO: Main view (Kanban / Gantt)</p>
       <CsvImportDialog />
+    </MainLayout>
   );
 }
